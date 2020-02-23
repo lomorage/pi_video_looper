2b9813e29b164e644ab8492dec69d71788be7a5d
Jeromy Fu <fuji246@gmail.com>
2020-02-23 01:14:48 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -1,71 +1,65 @@
-#!/bin/sh
+#!/bin/bash
 
 # Error out if anything fails.
 set -e
 
-# Make sure script is run as root.
-if [ "$(id -u)" != "0" ]; then
-  echo "Must be run as root with sudo! Try: sudo ./install.sh"
-  exit 1
-fi
-
+CUR_DIR=$(dirname "$0")
+pushd $CUR_DIR
 
 echo "Installing dependencies..."
 echo "=========================="
-apt update && apt -y install python3 python3-pip python3-pygame supervisor omxplayer ntfs-3g exfat-fuse
+sudo apt update && sudo apt -y install python3 python3-pip python3-pygame supervisor omxplayer ntfs-3g exfat-fuse
 
-apt -y install git build-essential python3-dev autoconf automake libtool
+sudo apt -y install git build-essential python3-dev autoconf automake libtool
 
 echo "Installing libde265..."
 echo "========================="
+cd ..
 git clone https://github.com/lomorage/libde265.git
 cd libde265
 ./autogen.sh
 ./configure --prefix=/opt/lomorage/lib
 make -j2
 make install
-cd ..
 
 echo "Installing libheif..."
 echo "========================="
+cd ..
 git clone https://github.com/lomorage/libheif.git
 cd libheif
 ./autogen.sh
 ./configure --prefix=/opt/lomorage/lib
 make -j2
 make install
-cd ..
 
 echo "Installing SDL_image..."
 echo "========================="
-apt -y install libsdl1.2-dev
+cd ..
+sudo apt -y install libsdl1.2-dev
 git clone https://github.com/lomorage/SDL_image.git
 cd SDL_image
 ./autogen.sh
 ./configure CFLAGS=-I/opt/lomorage/lib/include LDFLAGS=-L/opt/lomorage/lib/lib  --prefix=/opt/lomorage/lib
 make -j2
 make install
-cd ..
-
 echo "Installing video_looper program..."
 echo "=================================="
 
 # change the directoy to the script location
-cd "$(dirname "$0")"
+popd
+sudo mkdir -p /mnt/usbdrive0 # This is very important if you put your system in readonly after
+sudo mkdir -p /home/pi/video # create default video directory
 
-mkdir -p /mnt/usbdrive0 # This is very important if you put your system in readonly after
-mkdir -p /home/pi/video # create default video directory
+sudo pip3 install setuptools
+sudo python3 setup.py install --force
 
-pip3 install setuptools
-python3 setup.py install --force
-
-cp ./assets/video_looper.ini /boot/video_looper.ini
+sudo cp ./assets/video_looper.ini /boot/video_looper.ini
 
 echo "Configuring video_looper to run on start..."
 echo "==========================================="
 
-cp ./assets/video_looper.conf /etc/supervisor/conf.d/
+sudo cp ./assets/video_looper.conf /etc/supervisor/conf.d/
 
-service supervisor restart
+sudo service supervisor restart
 
 echo "Finished!"
